4e9829467742e8fabdf44bb28aeef2da91394a6c
Natalia2204 <loman75@mail.ru>
2024-01-18 15:45:07 -0500

    Added task12

---
+++ src/day03_practice_tasks/Task12.java
@@ -0,0 +1,22 @@
+package day03_practice_tasks;
+
+public class Task12 {
+    public static void main(String[] args) {
+
+        int num1 = 10;
+        int num2 = 20;
+
+
+        if (num1 != num2){
+            if (num1 > num2){
+                System.out.println(num1);
+            }else {
+                System.out.println(num2);
+            }
+        } else {
+            System.out.println("num1 = num2");
+        }
+
+    }
+
+}
