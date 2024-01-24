8d363c201052feb6be7dcca3c0c14ef04dc0303b
Natali Stolbovaia <loman75@mail.ru>
2024-01-24 18:12:08 -0500

    Task finished

---
+++ src/day01_practice_tasks/Birthday.java
@@ -0,0 +1,25 @@
+package day01_practice_tasks;
+
+import java.sql.SQLOutput;
+
+public class Birthday {
+    public static void main(String[] args) {
+
+        String name;
+        String birthDay;
+        String birthMonth;
+        String birthWear;
+
+        name = "John";
+        birthDay = "25";
+        birthMonth = "April";
+        birthWear = "1995";
+
+        System.out.println(name + " was born on " + birthMonth + " " + birthDay + ", " + birthWear);
+        System.out.println("5-2=" + 3);
+
+
+
+
+    }
+}
