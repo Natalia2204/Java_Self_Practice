1aa579ba25e759e50567d943fc55d1e9b21d2759
Natali Stolbovaia <loman75@mail.ru>
2024-01-31 20:34:13 -0500

    Changed

--- src/weekMoning/Carpet.java
+++ src/weekMoning/Carpet.java
@@ -7,12 +7,13 @@ public class Carpet {
     public double unitPrice;
     public boolean isPersian;
 
-    public void carpetInfo(double width, double length, double unitPrice, boolean isPersian) {
+    public Carpet(double width, double length, double unitPrice, boolean isPersian) {
         this.width = width;
         this.length = length;
         this.unitPrice = unitPrice;
         this.isPersian = isPersian;
     }
+
 
     public double calcCost() {
 
