1bddff828b1979e2616b3948680e25d1b790d5bf
Natali Stolbovaia <loman75@mail.ru>
2024-02-05 12:29:04 -0500

    Finished

---
+++ src/day11_practice_constructors/EmployeeClients.java
@@ -0,0 +1,10 @@
+package day11_practice_constructors;
+
+public class EmployeeClients {
+    public static void main(String[] args) {
+
+        Employee employee = new Employee("Jane", 28,'f', "SDET", 90000);
+        String infoEmployee = employee.work();
+
+    }
+}
