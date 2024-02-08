a78cdcec2ef1e8d1be9dec3282e59ef5b264194b
Natali Stolbovaia <loman75@mail.ru>
2024-02-07 21:45:10 -0500

    Finished

---
+++ src/day13_practice_Inheritance/PhoneClients.java
@@ -0,0 +1,24 @@
+package day13_practice_Inheritance;
+
+public class PhoneClients {
+    public static void main(String[] args) {
+
+        IPhone iPhone = new IPhone("Iphone", "13", "8", 900, "Black");
+        Samsung samsung = new Samsung("Samsung", "12", "9", 600, "White");
+        Nokia nokia =new Nokia("Nokia", "15A", "7", 280,"Red");
+
+        iPhone.call(1236787);
+        samsung.text(7895432);
+        nokia.faceTime(4569802);
+
+        System.out.println(iPhone.toString());
+        System.out.println(samsung.toString());
+        System.out.println(nokia.toString());
+
+    }
+}
+/*
+4. Create a class named "PhoneClients":
+   - Create multiple objects representing different phones.
+   - Test the methods and variables of each object.
+ */
